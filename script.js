c9e04a5b04f0865c189af662aba65abc5b97719c
Weissey <kelvinchangw@gmail.com>
2022-05-18 17:42:59 -0700

    Current version works barely, sum function concatenates as of now instead of getting the sum. Problem is understood, solution is yet to be implemented

--- script.js
+++ script.js
@@ -1,76 +1,163 @@
-const display = document.getElementById("display");
+const topDisplay = document.getElementById("topDisplay")
+const botDisplay = document.getElementById("botDisplay");
 const delBtn = document.getElementById("delBtn");
 const clearBtn = document.getElementById("clearBtn");
 
 const addBtn = document.getElementById("addBtn");
-const minusBts = document.getElementById("minusBtn");
+const minusBtn = document.getElementById("minusBtn");
 const multiplyBtn = document.getElementById("multiplyBtn");
 const divideBtn = document.getElementById("divideBtn");
 
 const numberBtns = document.querySelectorAll("[data-number]");
 const operatorBtns = document.querySelectorAll("[data-operator]")
 
-const DEFAULT_DISPLAY_VALUE = 0;
+const DEFAULT_DISPLAY_VALUE = "0";
 
 let currentValue = DEFAULT_DISPLAY_VALUE;
+let lastValue = DEFAULT_DISPLAY_VALUE;
 let currentOperator;
 
-
-console.log(numberBtns);
+console.log(operatorBtns);
 
 numberBtns.forEach((button) => {
-    button.addEventListener(("click"), () => appendNumber(button.textContent))
+    button.addEventListener(("click"), () => appendBotNumber(button.textContent));
 })
 
 operatorBtns.forEach((button) => {
-    button.addEventListener(("click"), () => setOperation(button.textContent))
+    button.addEventListener(("click"), () => setOperation(button.textContent));
 })
 
 operatorBtns.forEach((button) => {
-    button.addEventListener(("click"), () => this.classList.add("activatedBtn"))
+    button.addEventListener(("click"), activateButton);
 })
 
-function appendNumber(number) {
-    display.textContent += number;
-    currentValue = display.textContent
+function appendBotNumber(number) {
+    if (botDisplay.textContent === "0") {
+        botDisplay.textContent = "";
+    }
+    botDisplay.textContent += number;
+    currentValue = botDisplay.textContent
+    console.log("LAST VALUE IS " + lastValue)
+    console.log("CURRENT VALUE IS " + currentValue)
+}
+
+function appendTopNumber(number) {
+    topDisplay.textContent = number;
 }
 
 function setOperation(operator) {
-    switch (operator)
-    {
-        case "C":
-            clearDisplay();
-            break;
-            
-        case "DEL":
-            deleteNum();
-            break;
-
-        case "+":
-            activateButton(operator);
-            addNum();
-            break;
-
-        case "-":
-            minusNum();
-            break;
+    if (operator === "=") {
+        evaluate();
+        return;
+    } else if (operator === "DEL") {
+        deleteNum();
+        return;
+    } else if (operator === "C") {
+        clearBothDisplay();
+        return;
     }
-    currentOperator = operator;
+    lastValue = currentValue;
+    appendTopNumber(lastValue);
+    clearBotDisplay();
+    // switch (operator)
+    // {
+    //     case "+":
+    //         addNum(lastValue, currentValue);
+    //         break;
+
+    //     case "-":
+    //         minusNum();
+    //         break;
+
+    //     case "*":
+    //         multiplyNum();
+    //         break;
+
+    //     case "/":
+    //         divideNum();
+    // }
 }
 
-function clearDisplay() {
-    display.textContent = "";
+function clearBothDisplay() {
+    topDisplay.textContent = "";
+    botDisplay.textContent = "0";
+    deactivateBtns();
+}
+
+function clearTopDisplay() {
+    topDisplay.textContent = "";
+}
+
+function clearBotDisplay() {
+    botDisplay.textContent = "";
 }
 
 function deleteNum() {
-    display.textContent = display.textContent
+    if (botDisplay.textContent === "0") return;
+    botDisplay.textContent = botDisplay.textContent
         .toString()
-        .slice(0, -1)
+        .slice(0, -1);
+    if (botDisplay.textContent === "") return botDisplay.textContent = "0";
 }
 
-function addNum() {
+function addNum(a, b) {
+    clearBothDisplay();
+    return botDisplay.textContent = a + b;
+}
+
+function minusNum() {
+
+}
+
+function multiplyNum() {
+    
+}
+
+function divideNum() {
+
+}
+
+function evaluate() {
+    console.log(currentOperator.id)
+    if (currentOperator.id === "addBtn") {
+        addNum(lastValue, currentValue);
+    }
 }
 
 function activateButton() {
+    if (this === delBtn || this === clearBtn || this === equalBtn) return;
+
+    currentOperator = this;
     this.classList.add("activatedBtn")
+
+    console.log("CURRENT OPERATOR ID IS: " + currentOperator.id)
+
+    if (this === addBtn) {
+        minusBtn.classList.remove("activatedBtn");
+        multiplyBtn.classList.remove("activatedBtn");
+        divideBtn.classList.remove("activatedBtn");
+    } else if (this === minusBtn) {
+        addBtn.classList.remove("activatedBtn");
+        multiplyBtn.classList.remove("activatedBtn");
+        divideBtn.classList.remove("activatedBtn");
+    } else if (this === multiplyBtn) {
+        addBtn.classList.remove("activatedBtn");
+        minusBtn.classList.remove("activatedBtn");
+        divideBtn.classList.remove("activatedBtn");
+    } else if (this === divideBtn) {
+        addBtn.classList.remove("activatedBtn");
+        minusBtn.classList.remove("activatedBtn");
+        multiplyBtn.classList.remove("activatedBtn");
+    }
+}
+
+function deactivateBtns() {
+    addBtn.classList.remove("activatedBtn");
+    minusBtn.classList.remove("activatedBtn");
+    multiplyBtn.classList.remove("activatedBtn");
+    divideBtn.classList.remove("activatedBtn");
+}
+
+window.onload = () => {
+    botDisplay.textContent = DEFAULT_DISPLAY_VALUE;
 }
